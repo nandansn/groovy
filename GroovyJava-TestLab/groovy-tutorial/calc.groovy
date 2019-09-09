d23d32356ba1485bbfee82d89526e16a662a7184
Nandakumar R <nandakumar.rangasamy@oracle.com>
2019-09-09 09:57:27 +0530

    updated

---
+++ GroovyJava-TestLab/groovy-tutorial/calc.groovy
@@ -0,0 +1,13 @@
+a =10
+b =11
+
+c = a + b
+
+print(c)
+
+a = 'nanda'
+b = 'kumar'
+
+c = a + b
+
+print (c)
---
+++ GroovyJava-TestLab/groovy-tutorial/decision-making.groovy
@@ -0,0 +1,27 @@
+a = 10
+b = 11
+
+if (a > b) {
+    print(a)
+} else {
+    print(b)
+}
+
+day = 'SUNDAY'
+
+switch(day) {
+    case 'SUNDAY':
+
+        print('SUNDAY MORNING')    
+        break
+
+    case 'MONDAY':
+
+        print('MONDAY MORNING')
+        break
+
+    default:
+
+        print('Holiday')
+
+}
